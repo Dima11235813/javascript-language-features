aa6658eda2cf0590bf7e20cc815dd977ea240df5
Dmitri Larionov <LarionovDmitri@gmail.com>
2020-02-10 17:13:22 -0600

    added example of get set and extend with apply from docs

--- ProxyType/modules/proxyDemo.js
+++ ProxyType/modules/proxyDemo.js
@@ -1,6 +1,8 @@
 import { proxyTarget } from './proxyTarget.js'
 import { proxyHandler } from './proxyHandler.js'
 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy
+
 export const proxyDemo = () => {
     console.log(`Proxy demo module loaded`)
     let target = proxyTarget()
@@ -8,4 +10,6 @@ export const proxyDemo = () => {
     const p = new Proxy(target, handler)
     console.log("Proxy setup complete")
     console.table(p)
+    p.a = "A will now exist"
+    console.log(p.b) //Doesn't exist - proxy enables overriding what is returned if the object doesn't exist
 }
--- ProxyType/modules/proxyHandler.js
+++ ProxyType/modules/proxyHandler.js
@@ -1,5 +1,31 @@
 export const proxyHandler = () => {
     return {
+        set: function (obj, prop, value) {
+            if (prop === 'numberOfTimes') {
+                if (!Number.isInteger(value)) {
+                    throw new TypeError('The numberOfTimes property is not an integer');
+                }
+                if (value < 0) {
+                    throw new RangeError(`The property numberOfTimes can't be negative.`);
+                }
+            }
+            // The default behavior to store the value
+            obj[prop] = value;
+            // Indicate success
+            return true;
+        },
+        get: function (obj, prop) {
+            return prop in obj ? obj[prop] : "Doesn't exist"
+        },
+        construct: function (target, args) {
+            var obj = Object.create(base.prototype);
+            this.apply(target, obj, args);
+            return obj;
+        },
+        apply: function (target, that, args) {
+            sup.apply(that, args);
+            base.apply(that, args);
+        },
         handler: () => {
             console.log("Handling proxy")
         }
